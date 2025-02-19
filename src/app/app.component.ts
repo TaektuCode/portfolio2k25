4f8677faa26d5c469349581fea4bf993706b29cf
taektu_coding <taektu.coding@gmail.com>
2025-02-19 15:44:43 +0100

    setup: gerneral project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './shared-components/footer/footer.component';
+import { NavbarComponent } from './shared-components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio2k25';
---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ReferencesComponent } from './references/references.component';
+import { ContactComponent } from './contact/contact.component';
+
+@Component({
+  selector: 'app-main-content',
+  imports: [
+    HeroComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ReferencesComponent,
+    ContactComponent,
+  ],
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.scss',
+})
+export class MainContentComponent {}
