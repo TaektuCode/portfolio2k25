65a0eccae47b27fb82ddba247bdfc50c935106c2
taektu_coding <taektu.coding@gmail.com>
2025-02-20 10:46:58 +0100

    added: routerLink to /legal_notices

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,5 @@ import { ImprintComponent } from './imprint/imprint.component';
 
 export const routes: Routes = [
   { path: '', component: MainContentComponent },
-  { path: 'imprint', component: ImprintComponent },
+  { path: 'legal_notices', component: ImprintComponent },
 ];
