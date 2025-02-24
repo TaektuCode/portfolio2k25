87331478c38a882713955a41fcbd294c1aa3b4ca
taektu_coding <taektu.coding@gmail.com>
2025-02-24 09:20:13 +0100

    added: AboutMe content + animations

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -4,8 +4,21 @@ import { Component } from '@angular/core';
   selector: 'app-about-me',
   imports: [],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrl: './about-me.component.scss',
 })
 export class AboutMeComponent {
+  isAnimated: boolean = false;
+  isHovered: boolean = false;
 
+  showLine() {
+    if (!this.isAnimated) {
+      this.isHovered = true;
+      this.isAnimated = true;
+
+      setTimeout(() => {
+        this.isHovered = false;
+        this.isAnimated = false;
+      }, 3200);
+    }
+  }
 }
