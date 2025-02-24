2fe1e445832723bc419e5271e78aea00d2c02455
taektu_coding <taektu.coding@gmail.com>
2025-02-24 07:27:32 +0100

    added: Content to hero section + different svg icons

--- src/app/main-content/hero/hero.component.ts
+++ src/app/main-content/hero/hero.component.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
+  imageSrc: string = './assets/img/Passbild_skaliert.png';
+  buttonText: string = 'Hello World';
+  isLinkHovered: boolean = false;
+  isHovered: boolean = false;
 
+  changeButtonState(hover: boolean) {
+    this.isHovered = hover;
+  }
+
+  changeImage(hover: boolean) {
+    this.imageSrc = hover
+      ? './assets/img/Passbild_skaliert.png'
+      : './assets/img/Passbild_skaliert.png';
+  }
+
+  links: {
+    name: string;
+    image: string;
+    http: string;
+  }[] = [
+    {
+      name: 'LinkedIn',
+      image: './assets/icons/linkedIn.svg',
+      http: 'https://www.linkedin.com/in/alex-russotto-7a2r93/',
+    },
+    {
+      name: 'Github',
+      image: './assets/icons/Github.svg',
+      http: 'https://github.com/TaektuCode',
+    },
+    {
+      name: 'Mail',
+      image: './assets/icons/mail.svg',
+      http: 'mailto:russottoalex.studium@gmail.com',
+    },
+  ];
+
+  changeLinkHoverState(hover: boolean) {
+    this.isLinkHovered = hover;
+  }
 }
--- src/app/shared-components/navbar/navbar.component.ts
+++ src/app/shared-components/navbar/navbar.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  isScrolling = false;
+  isVisible = true; // Always visible at the start
+  private scrollTimeout: number | undefined;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollTop = window.scrollY;
+
+    this.isScrolling = scrollTop > 0;
+    this.isVisible = true; // Show navbar while scrolling
+
+    if (this.scrollTimeout) {
+      clearTimeout(this.scrollTimeout);
+    }
+
+    // Only hide if scrolled down (not at the top)
+    if (scrollTop > 0) {
+      this.scrollTimeout = window.setTimeout(() => {
+        this.isVisible = false; // Hide after 1.2s of no scrolling
+      }, 1200);
+    }
+  }
+}
