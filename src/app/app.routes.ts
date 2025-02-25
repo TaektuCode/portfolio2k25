ba9f909f91a20dd457cba359147d7851e5d4755d
taektu_coding <taektu.coding@gmail.com>
2025-02-25 06:32:57 +0100

    added: router setup for projects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { PokedexComponent } from './main-content/projects/pokedex/pokedex.component';
+import { GameComponent } from './main-content/projects/game/game.component';
+import { JoinComponent } from './main-content/projects/join/join.component';
 
 export const routes: Routes = [
   { path: '', component: MainContentComponent },
   { path: 'legal_notices', component: ImprintComponent },
+  { path: 'pokedex', component: PokedexComponent },
+  { path: 'game', component: GameComponent },
+  { path: 'join', component: JoinComponent },
 ];
