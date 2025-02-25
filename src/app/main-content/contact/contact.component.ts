b97dd0690e7bbe229b29f57e94c18c47e5e183c5
taektu_coding <taektu.coding@gmail.com>
2025-02-25 08:57:59 +0100

    added: contact section content

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  isHovered: boolean = false;
+  isAnimated: boolean = false;
+
+  showGraphic() {
+    if (!this.isAnimated) {
+      this.isHovered = true;
+      this.isAnimated = true;
+
+      setTimeout(() => {
+        this.isHovered = false;
+        this.isAnimated = false;
+      }, 2000);
+    }
+  }
+}
