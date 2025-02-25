fa1d7af6a72998cf97bbd5767a35a4b686fe7416
taektu_coding <taektu.coding@gmail.com>
2025-02-25 06:28:26 +0100

    added: references / collegues thoughts section

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -1,11 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-references',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './references.component.html',
-  styleUrl: './references.component.scss'
+  styleUrl: './references.component.scss',
 })
 export class ReferencesComponent {
-
+  cards: {
+    name: string;
+    image: string;
+    linkedIn: string;
+    text: string;
+    job: string;
+  }[] = [
+    {
+      name: 'Jannik',
+      image: './assets/graphics/references/cardA.png',
+      linkedIn: 'http/*',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.accusantium adipisci reprehenderit unde et!',
+      job: 'Frontend Developer',
+    },
+    {
+      name: 'Lea',
+      image: './assets/graphics/references/cardB.png',
+      linkedIn: 'http/*',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.accusantium adipisci reprehenderit unde et!',
+      job: 'Designer',
+    },
+    {
+      name: 'Jens',
+      image: './assets/graphics/references/cardA.png',
+      linkedIn: 'http/*',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.accusantium adipisci reprehenderit unde et!',
+      job: 'Project Manager',
+    },
+  ];
 }
