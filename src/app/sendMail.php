ffc31cb36c54ad588e65fabc617b8303cac39674
taektu_coding <taektu.coding@gmail.com>
2025-02-26 11:00:46 +0100

    added: sendMail.php

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'caryensong@googlemail.com';  
+            $recipient = 'russottoalex.studium@gmail.com';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
